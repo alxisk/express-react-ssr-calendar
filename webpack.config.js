1429090a4585871809f3f6e5001f24cee49d005b
alxisk <alexander.iskovskikh@gmail.com>
2018-11-12 22:05:09 +0300

    add src alias

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,25 @@ const bundlePath = path.resolve(__dirname, 'public')
 
 const clientConfig = {
   entry: ['./src/client/index.js'],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    },
+  },
   output: {
     path: bundlePath,
     filename: 'bundle.js',
     publicPath: '/',
   },
   module: {
-    rules: [{ test: /\.(js)$/, exclude: /node_modules/, use: 'babel-loader' }],
+    rules: [
+      { test: /\.(js)$/, exclude: /node_modules/, use: 'babel-loader' },
+      {
+        test: /\.(css|scss)$/,
+        exclude: /node_modules\/(?!normalize.css\/).*/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
   },
   devtool: process.env.NODE_ENV === 'production' ? null : 'eval-source-map',
   stats: 'minimal',
